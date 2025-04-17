8869f13ca754dd204a38d6a5adf40aad198f2c02
Eileen <eniedenfuehr@gmail.com>
2025-04-17 18:16:28 +0200

    add theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,8 @@ TIMEZONE = 'Europe/Rome'
 
 DEFAULT_LANG = 'en'
 
+THEME = "pelican-themes/graymill"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
